d655e9bdf7ec1348a0433fa6eb1b54231d79d335
goldendolphinresidence <michelvsouza@hotmail.com>
2024-12-07 11:02:47 -0300

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-
-<br><br>
-<a href="post_produto.php">1° Cadastra o Produto</a>
-<br><br>
-<a href="post_compra.php">2° Faz a Compra - Produto</a>
-<br><br>
-<a href="post_venda.php">3° Faz a Vendas - Produto</a>
-<br><br>
